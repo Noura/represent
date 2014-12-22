af29535b2cc1f56c0084482a892ebf4d44886088
Noura <culurienneldoreth@gmail.com>
2014-12-22 15:21:46 -0500

    added lorem ipsum text and styling

--- data.py
+++ data.py
@@ -2,33 +2,37 @@
 # to write this in python because when I mess up the syntax the error messages
 # are more informative
 
+# TODO stop using lorem ipsum stuff
+
+lorem_ipsum = open('lorem_ipsum.txt').read()
+
 people = [
     {
-        'name': 'Sarah Van Wart',
+        'name': 'Name Name',
         #'img_src': 'http://represent.berkeley.edu/__files/n624636990_1237310_4211.jpg',
         'img_src': '/static/img/from_dumpfm/wave.gif',
         'url': 'http://www.google.com',
     },
     {
-        'name': 'Meena Natarajan',
+        'name': 'Nama Nama',
         #'img_src': 'http://represent.berkeley.edu/__files/IMG_4868_2_2_2_3.jpg',
         'img_src': '/static/img/from_dumpfm/wave.gif',
         'url': 'http://www.google.com',
     },
     {
-        'name': 'Nick Merrill',
+        'name': 'Nom Nom',
         #'img_src': 'http://www.ischool.berkeley.edu/files/imagecache/profile-pic/nickmerrill.jpg',
         'img_src': '/static/img/from_dumpfm/wave.gif',
         'url': 'http://www.google.com',
     },
     {
-        'name': 'Noura Howell',
+        'name': 'Noo Nom',
         #'img_src': 'http://nourahowell.com/static/img/280px_tiles/at_sua_bali.jpg',
         'img_src': '/static/img/from_dumpfm/wave.gif',
         'url': 'http://www.google.com',
     },
     {
-        'name': 'Tapan Parikh',
+        'name': 'Ismi Ismi',
         #'img_src': 'http://represent.berkeley.edu/__files/gokarn6-1.jpg',
         'img_src': '/static/img/from_dumpfm/wave.gif',
         'url': 'http://www.google.com',
@@ -37,21 +41,21 @@ people = [
 
 projects = [
     {
-        'name': 'LocalGround',
+        'name': 'Project',
         'img_src': '/static/img/from_dumpfm/wave.gif',
-        'desc': 'A really awesome project',
+        'desc': lorem_ipsum,
         'url': 'http://www.google.com',
     },
     {
         'name': 'Another Project',
         'img_src': '/static/img/from_dumpfm/wave.gif',
-        'desc': 'Another really awesome project',
+        'desc': lorem_ipsum,
         'url': 'http://www.google.com',
     },
     {
         'name': 'Yet Another Project',
         'img_src': '/static/img/from_dumpfm/wave.gif',
-        'desc': 'this one is more chill',
+        'desc': lorem_ipsum,
         'url': 'http://www.google.com',
     },
 ]
